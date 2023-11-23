c6aa25e64a13b4be927c299f7127763df7a33ea4
MINAL-AHMED <mimalahmed6219@gmail.com>
2023-11-23 09:39:34 +0600

    object learn

---
+++ object/Object.js
@@ -0,0 +1,66 @@
+// let point = {
+//   x: 10,
+//   y: 20,
+// };
+// point.x = Math.round(Math.random() * 10);
+// console.log(point);
+
+// let obj = Object(); // --> Object constructor
+// obj.x = 10;
+// console.log(obj);
+
+// Access to object
+
+// let point = {
+//   math: 50,
+//   physics: 60,
+//   chemistry: 70,
+//   biology: 80,
+//   history: 90,
+//   geography: 100,
+// };
+
+// console.log(point["physics"]); //--> array notation
+// console.log(point.math);  //--> dot notation
+
+// compare two object
+
+// let obj1 = {
+//   x: 10,
+//   y: 20,
+// };
+
+// let obj2 = {
+//   x: 10,
+//   y: 20,
+// };
+
+// if (obj1.x === obj2.x && obj1.y === obj2.y) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+//object iteration
+// let obj = {
+//   x: 10,
+//   y: 20,
+//   z: 3,
+//   w: 40,
+// };
+
+// for (let i in obj) {
+//   console.log(obj[i]);
+// }
+
+//object method
+// let obj = {
+//   x: 10,
+//   y: 20,
+//   z: 3,
+//   w: 40,
+// };
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+// console.log(Object.entries(obj));
+// console.log(Object.assign({ a: 20 }, obj));
