ad0294f79de21e60add1ade35f0163cd87943c5f
Minal Ahmed <mimalahmed6219@gmail.com>
2024-01-13 20:17:25 +0600

    arrow function

--- es6/app.js
+++ es6/app.js
@@ -8,11 +8,29 @@
 
 // let is a local variable only accessing the current scope as,
 
-if (true) {
-  let x = 10;
-  console.log(x); // it's working
-}
+// if (true) {
+//   let x = 10;
+//   console.log(x); // it's working
+// }
 
 // console.log(x); // not accessible because use let variable
 
 //let and and const same but different is a value not changing the const variable
+
+//Normal function
+
+function sum(a, b) {
+  return a + b;
+}
+//  ==> it's a normal function
+console.log(sum(1, 2));
+
+//Arrow function with parameters
+
+let add = (a, b) => a + b; //Arrow function
+
+console.log(add(1, 2));
+
+let sqr = (x) => x * x;
+
+console.log(sqr(2));
