829266d849ac8988af510ea8c8276294162d1c08
Minal Ahmed <mimalahmed6219@gmail.com>
2024-01-21 02:35:42 +0600

    at least error solve

--- js_project/fullstack-weatherAPp/app.js
+++ js_project/fullstack-weatherAPp/app.js
@@ -12,8 +12,7 @@ const historyElm = document.getElementById("history");
 const masterHistory = document.getElementById("master-history");
 
 const API_KEY = "3c9af9a9e96abb7a8e1e5a72620142b0";
-const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?appid={API_KEY}
-`;
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?&appid=${API_KEY}`;
 const ICON_URL = "https://openweathermap.org/img/w/";
 const DEFAULT_CITY = "kushtia,bd";
 window.onload = function () {
@@ -25,48 +24,15 @@ window.onload = function () {
       getWeatherData();
     }
   );
-
-  axios
-    .get("/api/history")
-    .then(({ data }) => {
-      if (data.length > 0) {
-        updateHistory(data);
-      } else {
-        historyElm.innerHTML = "There is No History";
-      }
-    })
-    .catch((e) => {
-      console.log(e);
-      alert("Error Occurred");
-    });
-
-  cityInput.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-      if (e.target.value) {
-        getWeatherData(e.target.value, null, (weather) => {
-          e.target.value = "";
-          axios
-            .post("/api/history", weather)
-            .then(({ data }) => updateHistory(data))
-            .catch((e) => {
-              console.log(e);
-              alert("Error Occurred");
-            });
-        });
-      } else {
-        alert("Please Enter a Valid City Name");
-      }
-    }
-  });
 };
 
-function getWeatherData(city = DEFAULT_CITY, coords, cb) {
+function getWeatherData(city = DEFAULT_CITY, coords) {
   let url = BASE_URL;
 
   city === null
     ? (url = `${url}&lat=${coords.latitude}&lon=${coords.longitude}`)
     : (url = `${url}&q=${city}`);
-
+  console.log(url);
   axios
     .get(url)
     .then(({ data }) => {
@@ -80,23 +46,9 @@ function getWeatherData(city = DEFAULT_CITY, coords, cb) {
         pressure: data.main.pressure,
         humidity: data.main.humidity,
       };
-
-      setWeather(weather);
-      if (cb) cb(weather);
+      console.log(weather);
     })
     .catch((e) => {
       console.log(e);
-      alert("City Not Found");
     });
-}
-
-function setWeather(weather) {
-  condition.src = `${ICON_URL}${weather.icon}.png`;
-  city.innerHTML = weather.name;
-  country.innerHTML = weather.country;
-  mainText.innerHTML = weather.main;
-  description.innerHTML = weather.description;
-  temp.innerHTML = weather.temp;
-  pressure.innerHTML = weather.pressure;
-  humidity.innerHTML = weather.humidity;
 }
