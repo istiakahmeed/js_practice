40ac863a33498bb5d8621d720de4c4a4d09527a8
Minal Ahmed <mimalahmed6219@gmail.com>
2024-02-07 19:07:49 +0600

    all work done

--- js_project/snake-game/game.js
+++ js_project/snake-game/game.js
@@ -1,18 +1,31 @@
+import { draw as drawFood, update as updateFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
 import {
   SNAKE_SPEED,
   draw as drawSnake,
+  getSnakeHead,
+  snakeIntersection,
   update as updateSnake,
 } from "./snake.js";
-let lastRenderTime = 0;
 
+let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById("game-board");
 
 function main(currentTime) {
+  if (gameOver) {
+    if (confirm("You lost. Press ok to restart.")) {
+      window.location = "/";
+    }
+    return;
+  }
+
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
-  console.log("Render");
+
   lastRenderTime = currentTime;
+
   update();
   draw();
 }
@@ -21,9 +34,16 @@ window.requestAnimationFrame(main);
 
 function update() {
   updateSnake();
+  updateFood();
+  checkDeath();
 }
 
 function draw() {
   gameBoard.innerHTML = "";
   drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
