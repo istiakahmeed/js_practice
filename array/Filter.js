6d3fac94424c8bbef29ef537a9a6b5475c648dff
Minal Ahmed <mimalahmed6219@gmail.com>
2024-03-12 17:17:05 +0600

    implement filter method

--- array/Filter.js
+++ array/Filter.js
@@ -1,37 +1,57 @@
-// Imperative way
-const arr = [1, 2, 3, null, false, 4, 5, "", "test", 6, 7];
+// // Imperative way
+// const arr = [1, 2, 3, null, false, 4, 5, "", "test", 6, 7];
 
-count = 0;
-for (let i = 0; i < arr.length; i++) {
-  for (let j = i; j < arr.length - 1; j++) {
-    if (!arr[j] || typeof arr[j] !== "number") {
-      arr[j] = arr[j + 1];
-      arr[j + 1] = undefined;
+// count = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = i; j < arr.length - 1; j++) {
+//     if (!arr[j] || typeof arr[j] !== "number") {
+//       arr[j] = arr[j + 1];
+//       arr[j + 1] = undefined;
+//     }
+//   }
+
+//   if (arr[i] == undefined) {
+//     count++;
+//   }
+// }
+// arr.length -= count;
+
+// console.log(arr);
+
+// //Declarative way is easy
+
+// const filterArray = arr.filter((value) => typeof value === "number");
+
+// console.log(filterArray);
+
+// //easy to imperative way
+
+// let newArr = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (typeof arr[i] === "number") {
+//     newArr.push(arr[i]);
+//   }
+// }
+
+// console.log(newArr);
+
+// filter implement
+
+function myArray(arr, cb) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (cb(arr[i])) {
+      newArr.push(arr[i]);
     }
   }
-
-  if (arr[i] == undefined) {
-    count++;
-  }
-}
-arr.length -= count;
-
-console.log(arr);
-
-//Declarative way is easy
-
-const filterArray = arr.filter((value) => typeof value === "number");
-
-console.log(filterArray);
-
-//easy to imperative way
-
-let newArr = [];
-
-for (let i = 0; i < arr.length; i++) {
-  if (typeof arr[i] === "number") {
-    newArr.push(arr[i]);
-  }
+  return newArr;
 }
 
-console.log(newArr);
+let nam = ["a", "b", "c"];
+
+let res = myArray(nam, function (v) {
+  return v !== "b";
+});
+
+console.log(res);
